ea80a14465e0b80ad2eb474399f2c4ac2430ee11
r-gt <thiagoobregon000@gmail.com>
2026-06-15 16:46:39 -0300

    prueba

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sqlite3
-con = sqlite3.connect("data.db")
+database = sqlite3.connect("data.db")
 
+# instalar rich con "pip install rich"
+# en linux (probablemente) exija un argumento mas ""pip install rich --break-system-packages
 import rich
-
 
 
 if __name__ == '__main__':
 
 	print("test")
 #
+
+## un cambio
